d780107ead068b3de0a7e84ca422419fe16f4ee7
alexander <ajelesin@gmail.com>
2022-12-28 14:47:50 +0500

    day 23

---
+++ Day23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("release? (r) ");
+
+var lines = Console.ReadLine() == "r"
+    ? File.ReadLines("..\\..\\..\\input.txt")
+    : File.ReadLines("..\\..\\..\\debug.txt");
+
+f1(lines);
+Console.ReadLine();
+
+void f1(IEnumerable<string> lines)
+{
+
+}
